8c00077a7da41d4daff177d03d8199ec961aad0f
matt-maker <68969720+matt-maker@users.noreply.github.com>
2024-03-12 21:22:15 +0000

    second commit

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,6 @@ mod simulation;
 
 pub use crate::simulation::change_shade;
 
-const NUMBER_CELLS: usize = 100;
-const CELL_SIZE: f32 = 3.0;
-
 fn main() {
     nannou::app(model).update(update).run();
 }
@@ -24,21 +21,59 @@ impl Cell {
 
 pub struct Model {
     cells: Vec<Cell>,
+    cell_size: f32,
+
+    grid_size: i32,
+    dt: f32,
+    diff: f32,
+    visc: f32,
+
+    s: Vec<f32>,
+    density: Vec<f32>,
+
+    vx: Vec<f32>,
+    vy: Vec<f32>,
+    vz: Vec<f32>,
+
+    vx0: Vec<f32>,
+    vy0: Vec<f32>,
+    vz0: Vec<f32>,
 }
 
 fn model(app: &App) -> Model {
     let _window = app.new_window().size(512, 512).view(view).build().unwrap();
     let mut cells = Vec::new();
+    let grid_size: i32 = 100;
+    let cell_size: f32 = 3.0;
 
-    for y in 0..NUMBER_CELLS {
-        for x in 0..NUMBER_CELLS {
-            let pos_x: f32 = (x as f32 - (NUMBER_CELLS / 2) as f32) * CELL_SIZE;
-            let pos_y: f32 = (y as f32 - (NUMBER_CELLS / 2) as f32) * CELL_SIZE;
+    for y in 0..grid_size {
+        for x in 0..grid_size {
+            let pos_x: f32 = (x as f32 - (grid_size / 2) as f32) * cell_size;
+            let pos_y: f32 = (y as f32 - (grid_size / 2) as f32) * cell_size;
             let cell = Cell::new(vec2(pos_x, pos_y), 0.0);
             cells.push(cell);
         }
     }
-    Model { cells }
+
+    Model {
+        cells,
+        cell_size,
+        grid_size,
+        dt: 1.0 / 60.0,
+        diff: 3.0,
+        visc: 3.0,
+
+        s: vec![0.0; (grid_size * grid_size) as usize],
+        density: vec![0.0; (grid_size * grid_size) as usize],
+
+        vx: vec![0.0; (grid_size * grid_size) as usize],
+        vy: vec![0.0; (grid_size * grid_size) as usize],
+        vz: vec![0.0; (grid_size * grid_size) as usize],
+
+        vx0: vec![0.0; (grid_size * grid_size) as usize],
+        vy0: vec![0.0; (grid_size * grid_size) as usize],
+        vz0: vec![0.0; (grid_size * grid_size) as usize],
+    }
 }
 
 fn update(app: &App, model: &mut Model, _update: Update) {
@@ -51,8 +86,8 @@ fn view(app: &App, model: &Model, frame: Frame) {
 
     for cell in model.cells.iter() {
         draw.rect()
-            .width(CELL_SIZE)
-            .height(CELL_SIZE)
+            .width(model.cell_size)
+            .height(model.cell_size)
             .color(Hsl::new(0.0, 0.0, cell.shade))
             .xy(cell.position);
     }
